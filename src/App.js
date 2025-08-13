7dea9e764b25b86a528c75969b907e6a1171a648
ksuarez032025 <kbsuarez626@gmail.com>
2025-08-13 12:00:36 -0400

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Weather from "./Weather";
-import "./App.css";
+import "./css/App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="San Francisco" />
+        <Weather defaultCity="Buffalo" />
       </div>
     </div>
   );
